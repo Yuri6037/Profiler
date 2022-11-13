c852a0e3b7d423c1c7b22da97d369ee7918870f0
Yuri Edward <yuri6037@outlook.com>
2022-11-13 20:56:02 +0100

    Initial commit with initial version of CoreData model

--- Profiler/ContentView.swift
+++ Profiler/ContentView.swift
@@ -1,9 +1,25 @@
+// Copyright 2022 Yuri6037
 //
-//  ContentView.swift
-//  Profiler
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
 //
-//  Created by Yuri Edward on 11/13/22.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS
+// IN THE SOFTWARE.
 
 import SwiftUI
 import CoreData
@@ -12,39 +28,34 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Project.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var items: FetchedResults<Project>
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Item at \(item.timestamp!, formatter: dateFormatter)")
+                        Text("Test: \(item.name!)")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("\(item.name!)").bold()
+                                Text("(\(item.version ?? "No version"))")
+                            }
+                            Text("\(item.timestamp!, formatter: dateFormatter)")
+                        }
                     }
                 }
                 .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
             }
             Text("Select an item")
         }
     }
 
-    private func addItem() {
+    /*private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
@@ -58,7 +69,7 @@ struct ContentView: View {
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
-    }
+    }*/
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
@@ -76,7 +87,7 @@ struct ContentView: View {
     }
 }
 
-private let itemFormatter: DateFormatter = {
+private let dateFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.dateStyle = .short
     formatter.timeStyle = .medium
--- Profiler/Persistence.swift
+++ Profiler/Persistence.swift
@@ -1,9 +1,25 @@
+// Copyright 2022 Yuri6037
 //
-//  Persistence.swift
-//  Profiler
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
 //
-//  Created by Yuri Edward on 11/13/22.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS
+// IN THE SOFTWARE.
 
 import CoreData
 
@@ -14,10 +30,20 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let project = Project(context: viewContext);
+            project.id = UUID();
+            project.name = "Test";
+            project.timestamp = Date();
+            let node = SpanNode(context: viewContext);
+            node.project = project;
+            node.path = "root";
+            node.metadata = SpanMetadata(context: viewContext);
+            node.metadata?.id = UUID();
+            node.metadata?.name = "test";
+            node.metadata?.target = "test target";
+            node.metadata?.level = 0;
         }
-        do {
+       do {
             try viewContext.save()
         } catch {
             // Replace this implementation with code to handle the error appropriately.
