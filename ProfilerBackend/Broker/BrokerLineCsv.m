b3c864023266a3da618b86d0e6b3f3a2c2979586
Yuri Edward <yuri6037@outlook.com>
2022-12-19 11:22:35 +0100

    Added BrokerLineCsv

---
+++ ProfilerBackend/Broker/BrokerLineCsv.m
@@ -0,0 +1,52 @@
+// Copyright 2022 Yuri6037
+//
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS
+// IN THE SOFTWARE.
+
+#import "BrokerLineCsv.h"
+
+@implementation BrokerLineCsv {
+    CSVRow _csvData;
+    CSVParser *_parser;
+}
+
+@synthesize csvData = _csvData;
+
+- (instancetype)init {
+    _csvData = nil;
+    _parser = [[CSVParser alloc] init:','];
+    return [super init];
+}
+
+- (instancetype)initWithSeparator:(char)separator {
+    _csvData = nil;
+    _parser = [[CSVParser alloc] init:separator];
+    return [super init];
+}
+
+- (BOOL)parse:(NSString *)str withError:(NSError **)error {
+    if (![super parse:str withError:error])
+        return NO;
+    _csvData = [_parser parseRow:super.data];
+    return YES;
+}
+
+@end
